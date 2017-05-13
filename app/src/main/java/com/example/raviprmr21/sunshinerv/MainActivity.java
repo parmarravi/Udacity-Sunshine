766f7781a9effe411e5139a26409bc440e09ba11
parmarravi <parmarravi950@gmail.com>
2017-05-13 15:54:23 +0530

    udacity tutorial

---
+++ app/src/main/java/com/example/raviprmr21/sunshinerv/MainActivity.java
@@ -0,0 +1,74 @@
+package com.example.raviprmr21.sunshinerv;
+
+import android.content.Context;
+import android.content.Intent;
+import android.support.v4.app.Fragment;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.ContextMenu;
+import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        //The savedInstanceState is a reference to a Bundle object that is passed into the onCreate method of every Android Activity.
+        // Activities have the ability, under special circumstances, to restore themselves to a previous state using the data stored in this bundle
+
+        if (savedInstanceState == null)
+        {
+        
+            getSupportFragmentManager().beginTransaction().add(R.id.container, new ForecastFragment()).commit();
+
+        }
+
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            Intent intent = new Intent(this,SettingActivity.class);
+            startActivity(intent);
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+
+}
+
---
+++ app/src/main/java/com/example/raviprmr21/sunshinerv/SettingActivity.java
@@ -0,0 +1,82 @@
+package com.example.raviprmr21.sunshinerv;
+
+
+import android.preference.ListPreference;
+import android.preference.Preference;
+import android.preference.PreferenceActivity;
+import android.preference.PreferenceFragment;
+import android.preference.PreferenceManager;
+import android.support.v7.app.AppCompatActivity;
+import com.example.raviprmr21.sunshinerv.R;
+import android.os.Bundle;
+
+public class SettingActivity extends PreferenceActivity implements Preference.OnPreferenceChangeListener {
+
+    // make a class to use PreferenceFragment because addPreferencesFromResource and findPreferencedeprecated in PreferenceActivity
+    public static class Presf extends PreferenceFragment {
+        @Override
+        public void onCreate(Bundle savedInstanceState) {
+            super.onCreate(savedInstanceState);
+
+            addPreferencesFromResource(R.xml.pref_general);
+
+            // For all preferences, attach an OnPreferenceChangeListener so the UI summary can be
+            // updated when the preference changes.
+
+            //make new class from SettingsActivity because bindPreferenceSummaryToValue is nonstatic class
+//and now I can use that by new class
+            SettingActivity m = new SettingActivity();
+            m.bindPreferenceSummaryToValue(findPreference(getString(R.string.pref_location_key)));
+
+        }
+
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        // Add 'general' preferences, defined in the XML file
+
+// finaly for setup Prefrence I used this and replace that by new Prefs Class, Hoya its Worked and data input via user is change and weather is update :D
+        getFragmentManager().beginTransaction().replace(android.R.id.content, new Presf()).commit();
+
+
+    }
+
+    /**
+     * Attaches a listener so the summary is always updated with the preference value.
+     * Also fires the listener once, to initialize the summary (so it shows up before the value
+     * is changed.)
+     */
+    private void bindPreferenceSummaryToValue(Preference preference) {
+        // Set the listener to watch for value changes.
+        preference.setOnPreferenceChangeListener(this);
+
+        // Trigger the listener immediately with the preference's
+        // current value.
+        onPreferenceChange(preference,
+                PreferenceManager
+                        .getDefaultSharedPreferences(preference.getContext())
+                        .getString(preference.getKey(), ""));
+    }
+
+    @Override
+    public boolean onPreferenceChange(Preference preference, Object value) {
+        String stringValue = value.toString();
+
+        if (preference instanceof ListPreference) {
+            // For list preferences, look up the correct display value in
+            // the preference's 'entries' list (since they have separate labels/values).
+            ListPreference listPreference = (ListPreference) preference;
+            int prefIndex = listPreference.findIndexOfValue(stringValue);
+            if (prefIndex >= 0) {
+                preference.setSummary(listPreference.getEntries()[prefIndex]);
+            }
+        } else {
+            // For other preferences, set the summary to the value's simple string representation.
+            preference.setSummary(stringValue);
+        }
+        return true;
+    }
+
+}
